818333fe17b5e5cedf374253d76228d63827d2ca
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-09-23 13:57:06 +0200

    task 02 done

--- 02_deleting_an_item_from_an_array_using_filter/script.js
+++ 02_deleting_an_item_from_an_array_using_filter/script.js
@@ -1,6 +1,23 @@
 let items = [
-    { id: 1, letter: "a" },
-    { id: 2, letter: "b" },
-    { id: 3, letter: "c" },
-    { id: 4, letter: "d" },
+  { id: 1, letter: "a" },
+  { id: 2, letter: "b" },
+  { id: 3, letter: "c" },
+  { id: 4, letter: "d" },
 ];
+console.log("before", items);
+
+function removeItem(idToRemove) {
+  // filter methdod to create a new array based on some condition
+  const filteredItems = items.filter(item => {
+    console.log(item.id !== idToRemove);
+    if (item.id !== idToRemove) {
+      return true;
+    }
+  })
+
+  items = filteredItems;
+}
+
+removeItem(4);
+
+console.log("after", items);
