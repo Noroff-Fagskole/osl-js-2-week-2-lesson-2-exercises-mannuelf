96d8c51d4c6a25734b4f231887da44f3f4683189
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-09-23 14:44:23 +0200

    task 04 done

--- 04_creating-a-list-from-an-array/js/script.js
+++ 04_creating-a-list-from-an-array/js/script.js
@@ -9,7 +9,7 @@ function addToList() {
   const newItem = listInput.value.trim();
 
   if (newItem.length >= 2) {
-    listItems.push(newItem);
+    listItems.push(newItem); // adding too
     createList();
     listInput.value = "";
     console.log(listItems);
@@ -20,17 +20,37 @@ function createList() {
   const listContainer = document.querySelector("ul");
 
   listContainer.innerHTML = "";
-
+  console.log("createList", listItems);
   listItems.forEach(function(item) {
     listContainer.innerHTML += `
-      <li><span>${item}</span>`;
+      <li>
+        <span>${item}</span>
+        <i class="fa fa-trash" data-item="${item}"></i>
+      </li>`;
   });
 
   const items = document.querySelectorAll("li span");
-
   items.forEach(function (item) {
     item.addEventListener("click", handleClick);
   });
+
+  const trashCans = document.querySelectorAll("li i");
+  trashCans.forEach(can => {
+    can.addEventListener("click", removeFromList);
+  })
+}
+
+function removeFromList(e) {
+  const deletedItem = e.target.dataset.item;
+  // filter on existing array & exclude the deleted item which was clicked
+  const newItemsArray = listItems.filter(item => {
+     if (deletedItem !== item) {
+       return true;
+     }
+   })
+   listItems = newItemsArray; // reassign listItems with new filtered Array
+   console.log("removeFromList", listItems );
+   createList(); // rerender list with new items
 }
 
 function handleClick(event) {
