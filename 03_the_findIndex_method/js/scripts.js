ff1e8d48bf76f6031e363c6199f1e2079328ea6d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-09-23 14:16:33 +0200

    task 03 done

--- 03_the_findIndex_method/js/scripts.js
+++ 03_the_findIndex_method/js/scripts.js
@@ -3,3 +3,24 @@ const products = [
   { id: 2, name: "Nike Cortez", price: "2500", is_sale: false },
   { id: 3, name: "New Balance", price: "1500", is_sale: true }
 ];
+
+/*
+for (let i = 0; i < products.length; i++ ) {
+  console.log(products[i]);
+}*/
+
+// findIdex()
+const itemIndex = products.findIndex(product => {
+  if (product.id === 1) {
+    return true;
+  }
+});
+
+console.log(itemIndex);
+
+const specificItem = products[itemIndex];
+console.log(specificItem);
+
+products[itemIndex].name = "Adidas Stan Smiths"; // update name
+products[itemIndex].price = "500"; // update name
+console.log(products);
